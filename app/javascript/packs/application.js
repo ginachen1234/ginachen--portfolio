b3795062cce9ef6d1e82f9e89ec9150f88c69ac9
gina chen <ginachen588@gmail.com>
2019-03-29 15:12:40 +0000

    projects added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -44,8 +44,15 @@ $(document).ready(function(){
   });
 
 });
-//skills & resume
-
-
-
+//footer
+var instaFeed = new Instafeed({
+              get: 'user',
+              userId: 34139106,
+              accessToken: '34139106.467ede5.2e8f6b6a9cf846f6bd23169b624aca22',
+              target: 'instafeed',
+              sortBy: 'most-liked',
+              limit: 200,
+              resolution: 'thumbnail'
+}); instaFeed.run();
+//
 
